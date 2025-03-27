fd2b0a843e3f7367141a430cca446959216a4c12
Isabella Tang <Isabella.tang@arcadis.com>
2025-03-26 22:54:50 -0400

    add migrations back to gitignore

--- prisma/migrations/20250318111042_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "QuizAnswer" (
-    "id" SERIAL NOT NULL,
-    "questionId" INTEGER NOT NULL,
-    "selectedOption" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "QuizAnswer_pkey" PRIMARY KEY ("id")
-);
